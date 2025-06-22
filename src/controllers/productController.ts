40fcb26f3b4e7fb85526aed8f3319ca8669ac566
Shaheed mon <sheheedk07@gmail.com>
2025-06-22 23:57:00 +0530

    final commit

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,59 +1,119 @@
-import { Request,Response } from "express"
-import {v2 as cloudinary} from 'cloudinary'
+import { Request, Response } from "express";
+import { v2 as cloudinary } from "cloudinary";
+import productModel from "../models/ProductModel";
 
-
-
-
-// function add products 
-const addProducts=async (req:Request,res:Response):Promise<void>=>{
-try {
+// function add products
+const addProducts = async (req: Request, res: Response): Promise<void> => {
+  try {
     const files = req.files as {
       [fieldname: string]: Express.Multer.File[];
     };
-    const {name,description,price,category,subCategory,sizes,bestSeller}=req.body
+    const {
+      name,
+      description,
+      price,
+      category,
+      subCategory,
+      sizes,
+      bestSeller,
+    } = req.body;
 
-    const image1 = files.image1&& files.image1?.[0];
-    const image2 = files.image2&& files.image2?.[0];  
-    const image3 = files.image3&& files.image3?.[0];
-    const image4 = files.image4&& files.image4?.[0];
-    
-const images=[image1,image2,image3,image3].filter((item)=>item!==undefined)
+    const image1 = files.image1 && files.image1?.[0];
+    const image2 = files.image2 && files.image2?.[0];
+    const image3 = files.image3 && files.image3?.[0];
+    const image4 = files.image4 && files.image4?.[0];
 
-const imagesurl=await Promise.all(
-    images.map((async(items,index)=>{
-        let result=await cloudinary.uploader.upload(items.path,{resource_type:'image'})
-        return result.secure_url
-    }))
-)
-console.log(name,description,price,category,subCategory,sizes,bestSeller)
-console.log(images);
-res.json({})
+    const images = [image1, image2, image3, image4].filter(
+      (item) => item !== undefined
+    );
 
-} catch (error) {
-    console.log(error)
-   if (error instanceof Error) {
+    const imagesUrl = await Promise.all(
+      images.map(async (items, index) => {
+        let result = await cloudinary.uploader.upload(items.path, {
+          resource_type: "image",
+        });
+        return result.secure_url;
+      })
+    );
+    const productData = {
+      name,
+      description,
+      category,
+      price: Number(price),
+      subCategory,
+      bestSeller: bestSeller === "true" ? true : false,
+      size: JSON.parse(sizes),
+      images: imagesUrl,
+      date: Date.now(),
+    };
+    console.log(productData);
+
+    const product = new productModel(productData);
+    await product.save();
+
+    res.json({ success: true, message: "Product Added" });
+  } catch (error) {
+    console.log(error);
+    if (error instanceof Error) {
       res.status(400).json({ success: false, message: error.message });
     } else {
       res
         .status(400)
         .json({ success: false, message: "An unknown error occurred" });
     }
-}
-}
+  }
+};
 
-// function for list products 
-const listProducts=async (req:Request,res:Response):Promise<void>=>{
+// function for list products
+const listProducts = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const products = await productModel.find({});
+    res.json({ success: true, products });
+  } catch (error) {
+    console.log(error);
+    if (error instanceof Error) {
+      res.status(400).json({ success: false, message: error.message });
+    } else {
+      res
+        .status(400)
+        .json({ success: false, message: "An unknown error occurred" });
+    }
+  }
+};
 
-}
+// function for remove product
+const removeProduct = async (req: Request, res: Response): Promise<void> => {
+  try {
+    await productModel.findByIdAndDelete(req.body.id);
+    res.json({ success: true, message: "Product Removed" });
+  } catch (error) {
+    console.log(error);
+    if (error instanceof Error) {
+      res.status(400).json({ success: false, message: error.message });
+    } else {
+      res
+        .status(400)
+        .json({ success: false, message: "An unknown error occurred" });
+    }
+  }
+};
 
-// function for remove product 
-const removeProduct=async (req:Request,res:Response):Promise<void>=>{
+// function single product info
+const singleProduct = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { productId } = req.body;
+    const product = await productModel.findById(productId);
+    res.json({ success: true, product });
+  } catch (error) {
+    console.log(error);
+    if (error instanceof Error) {
+      res.status(400).json({ success: false, message: error.message });
+    } else {
+      res
+        .status(400)
+        .json({ success: false, message: "An unknown error occurred" });
+    }
+  }
+};
 
-}
-
-// function single product info 
-const singleProduct=async (req:Request,res:Response):Promise<void>=>{
-
-}
-
-export {addProducts,listProducts,removeProduct,singleProduct}
+export { addProducts, listProducts, removeProduct, singleProduct };
--- src/models/ProductModel.ts
+++ src/models/ProductModel.ts
@@ -4,7 +4,7 @@ interface Product extends Document {
   name: string;
   description: string;
   price: number;
-  image: string[];       
+  images: { type: [String], required: true },    
   category: string;
   subCategory: string;
   size: string[];        
@@ -16,7 +16,7 @@ const productSchema = new mongoose.Schema<Product>({
   name: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
-  image: { type: [String], required: true },      
+  images: { type: [String], required: true },      
   category: { type: String, required: true },
   subCategory: { type: String, required: true },
   size: { type: [String], required: true },
